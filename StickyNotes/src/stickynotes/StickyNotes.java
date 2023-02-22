4ba844930294958bf8d3723706c103f9e3bd0cf3
Nathan Wu <113073109+L3g3ndari@users.noreply.github.com>
2023-02-22 14:08:39 -0800

    Added the extra file. Previous commit still stands. - Nathan

--- StickyNotes/src/stickynotes/StickyNotes.java
+++ StickyNotes/src/stickynotes/StickyNotes.java
@@ -1,9 +1,25 @@
 package stickynotes;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
 public class StickyNotes {
+	
+	JFrame console = new JFrame("NOTES CONSOLE");
+	NotesPanel panel = new NotesPanel();
+	
+	public StickyNotes() {
+		console.setSize(600, 800);
+		console.setLayout(new BorderLayout());
+		console.add(panel, BorderLayout.CENTER);
+		
+		console.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		console.setVisible(true);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new StickyNotes();
 
 	}
 
